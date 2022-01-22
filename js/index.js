e6bd05ca897de2eb8d3c1a356cd5de60f747a700
elidvenega <venegaelid@gmail.com>
2022-01-22 10:54:23 -0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,6 @@
+//   API key and variables
+const apiUrl =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=f2342a6e3901fac463896c0b0839dad9&page=1";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI =
+  "https://api.themoviedb.org/3/search/movie?&api_key=f2342a6e3901fac463896c0b0839dad9&query=";
