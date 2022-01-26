d92bcb31400423f15890136aa4e37cd88dae0c4e
elidvenega <venegaelid@gmail.com>
2022-01-25 22:57:21 -0800

    finish project

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ before writing the actual code.
 //API key and variables
 const API =
   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=f2342a6e3901fac463896c0b0839dad9&page=1";
-const IMGP = "https://image.tmdb.org/t/p/w1280";
+const IMG = "https://image.tmdb.org/t/p/w1280";
 const SEARCH =
   "https://api.themoviedb.org/3/search/movie?&api_key=f2342a6e3901fac463896c0b0839dad9&query=";
 
@@ -23,14 +23,41 @@ const searchClass = document.getElementsByClassName("search");
 const main = document.getElementById("main");
 
 /* Function to fetch data */
-const getData = function (api) {
+const getMovies = function (api) {
   fetch(api)
     .then((resp) => resp.json())
-    .then((data) => console.log(data));
-  // create a loop to get content and append it to #main div
-  data.forEach((element) => {});
+    .then((data) => {
+      console.log(data.results);
+      data.results.forEach((element) => {
+        // creating elements for movies
+        const el = document.createElement("div");
+        const image = document.createElement("img");
+
+        image.src = IMG + element.poster_path;
+        el.appendChild(image);
+
+        main.appendChild(el);
+      });
+    });
 };
 
-getData(API);
+getMovies(API);
 
 /* Event lisener to filter throug data */
+// retieve data and append to form
+// research event listeners
+// google how to add data
+// Make best attemp to fix this
+
+// searches for movie
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  main.innerHTML = "";
+
+  const searchTerm = search.value;
+
+  if (searchTerm) {
+    getMovies(SEARCH + searchTerm);
+    search.value = "";
+  }
+});
